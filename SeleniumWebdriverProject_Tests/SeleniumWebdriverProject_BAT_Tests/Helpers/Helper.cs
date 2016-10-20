efc0017261a49d9ae9ac6758ce7aeb53f1addd9b
sujithvakathanam-digiterre <Sujithv@digiterre.com>
2016-10-20 16:37:15 +0100

    first commit

---
+++ SeleniumWebdriverProject_Tests/SeleniumWebdriverProject_BAT_Tests/Helpers/Helper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumWebdriverProject_Tests.Helpers
+{
+    public class Helper
+    {
+        public  IWebDriver Driver;
+        public string BaseUrl;
+        
+
+        public Helper(string browser)
+        {
+            BaseUrl = ConfigurationManager.AppSettings["BaseUrl"];
+            
+            GetDriver(browser);
+        }
+ 
+     
+        public void GetDriver(string browser)
+        {
+
+            DesiredCapabilities capability = null;
+            switch (browser)
+            {
+
+                case "firefox":
+
+                    capability = DesiredCapabilities.Firefox();
+
+                    break;
+
+                case "phantomjs":
+
+                    capability = DesiredCapabilities.PhantomJS();
+
+                    break;
+
+                case "chrome":
+
+                    capability = DesiredCapabilities.Chrome();
+
+                    break;
+
+                case "ie":
+
+                    capability = DesiredCapabilities.InternetExplorer();
+
+                    break;
+
+                default:
+
+                    capability = DesiredCapabilities.Firefox();
+
+                    break;
+            }
+            //capability.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            //var remotehost = new Uri("http://localhost:4444/wd/hub");
+
+
+            capability.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Linux));
+            var remotehost = new Uri("http://192.168.99.100:4444/wd/hub");
+            
+            Driver = new RemoteWebDriver(remotehost, capability);         
+
+        }
+
+        
+        public void GetSessionId()
+        {
+            var sessionIdProperty = typeof(RemoteWebDriver).GetProperty("SessionId", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (sessionIdProperty != null)
+            {
+                SessionId sessionId = sessionIdProperty.GetValue(Driver, null) as SessionId;
+                if (sessionId == null)
+                {
+                    Trace.TraceWarning("Could not obtain SessionId.");
+                }
+                else
+                {
+                    Trace.TraceInformation("SessionId is " + sessionId.ToString());
+                    
+                }
+            }
+        }
+
+   
+    }
+}
---
+++ SeleniumWebdriverProject_Tests/SeleniumWebdriverProject_BAT_Tests/Tests/JoesCoffeeHomePage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using NUnit.Framework;
+using System.Threading;
+using OpenQA.Selenium;
+using SeleniumWebdriverProject_Tests.Helpers;
+
+
+namespace SeleniumWebdriverProject_Tests
+{
+    [TestFixture]
+    [Parallelizable()]
+    public class HeroPage 
+    {
+        private IWebDriver _driver;
+        private Helper helper;
+        private static string[] browsers = ConfigurationManager.AppSettings["Browsers"].Split(',');
+        private string browser;
+
+        public static IEnumerable<string> GetBrowser
+        {
+            get
+            {
+                foreach (var selectedBrowser in browsers)
+                {
+                    yield return selectedBrowser;
+                }
+            }
+        }
+
+        //[Factory("GetBrowser")]
+        //public HeroPage(string browser)
+        //{
+        //    this.browser = browser;
+        //}
+
+
+        //[TestCase("GetBrowser")]
+        public HeroPage()
+        {
+            this.browser = "chrome";
+        }
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            helper = new Helper(browser);
+            _driver = helper.Driver;
+        }
+
+
+        [Test]
+        public void HomePage_Title()
+        {
+            //_driver.Navigate().GoToUrl(helper.BaseUrl);
+            _driver.Navigate().GoToUrl("http://localhost:24847");
+            var pageSource = _driver.PageSource;
+
+            Thread.Sleep(5000);
+            string title= _driver.FindElement(By.CssSelector("#welcometext>header")).Text;
+
+
+            Assert.AreEqual(title, "Joe's Coffee Store");
+            helper.GetSessionId();
+        }
+
+
+        [Test]
+        public void HomePage_Title1()
+        {
+            _driver.Navigate().GoToUrl(helper.BaseUrl);
+            string title = helper.Driver.FindElement(By.XPath(".//*[@id='headername']")).Text;
+            Assert.AreEqual(title, "Joe's Coffee Store");
+            helper.GetSessionId();
+        }
+
+
+        [Test]
+        public void HomePage_Title2()
+        {
+            _driver.Navigate().GoToUrl(helper.BaseUrl);
+            string title = helper.Driver.FindElement(By.XPath("//*[@id='headername']")).Text;
+            Assert.AreEqual(title, "Joe's Coffee Store1");
+            helper.GetSessionId();
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+
+            Console.WriteLine("Teardown SessionId");
+            //helper.GetSessionId();
+            helper.Driver.Quit();
+        }
+    }
+}
