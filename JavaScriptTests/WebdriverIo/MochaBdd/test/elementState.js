fac68dbd0091455dfc234dc2390257cc79efcb67
Lambert Egbe <lamberte@digiterre.com>
2018-10-10 14:42:45 +0100

    adding chai assertions to javascript tests

--- JavaScriptTests/WebdriverIo/MochaBdd/test/elementState.js
+++ JavaScriptTests/WebdriverIo/MochaBdd/test/elementState.js
@@ -1,17 +1,34 @@
 
+var chai  = require("chai");
+var expect = chai.expect;
+
 describe('Element State Feature', function(){
 
     beforeEach(function (){
         browser.url('https://www.amazon.co.uk/');
+        browser.windowHandleMaximize();    
     });
 
     it('IsVisible Test', function(){
-        var isVisible = browser.isVisible('nav-shop');
-        //expect(isVisible, "element exists").to.be.true();
+        var hasVisible = browser.isVisible('#nav-your-amazon');
+        expect(hasVisible, "element is not visible").to.be.true;
     });
 
     it('IsExisting Test', function(){
-        var hasVisible = browser.isExisting('nav-shop');
-        //expect(hasVisible, "element is visible").to.be.true();
+        var hasVisible = browser.isVisible('#nav-your-amazon');
+        expect(hasVisible, "element does not exist").to.be.true;
+    });
+
+    it('HasFocus Test', function(){
+        var hasVisible = browser.hasFocus('#nav-your-amazon');
+        expect(hasVisible, "element does not exist").to.be.false;
+    });
+
+    it('WaitFor Test', function(){
+        //ToDo
+    });
+
+    it('WaitUntil Test', function(){
+        //ToDo
     });
 });
